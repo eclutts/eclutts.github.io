88070ac31e6d21a12eaae64e4b0af40dd645aed5
luttsec <luttsec@gmail.com>
2022-08-13 14:02:16 -0400

    added rest of simple website

---
+++ README.md
@@ -0,0 +1,2 @@
+# simple_website
+Making a simple website, learning the basics of HTML, CSS, and JavaScript
---
+++ php/test5_get.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>phptest</title>
+    </head> 
+        
+    <body>
+        <?php
+        echo $_POST["sum1"];
+        ?>
+    </body>
+</html>
